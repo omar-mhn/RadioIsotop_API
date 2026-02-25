8131ee0a83c8d1aada843a3c1cefeed434385b9c
omar <omar@>
2026-02-25 16:27:12 +0100

    First commit

---
+++ radioisotopo/src/main/java/com/projecte/radioisotopo/RadioisotopoApplication.java
@@ -0,0 +1,13 @@
+package com.projecte.radioisotopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioisotopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioisotopoApplication.class, args);
+	}
+
+}
