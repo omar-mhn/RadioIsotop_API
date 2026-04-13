c1e60c0f6cc381b022c2006053ad8205b3940899
omar <omar@>
2026-04-13 18:21:42 +0200

    Admin Permisos

--- radioisotopo/src/main/java/com/projecte/radioisotopo/Security/SecurityConfiguration.java
+++ radioisotopo/src/main/java/com/projecte/radioisotopo/Security/SecurityConfiguration.java
@@ -31,6 +31,7 @@ public class SecurityConfiguration {
                 // Permitimos el acceso total a las rutas de autenticación (Login/Registro)
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                 
                 // Cualquier otra petición debe estar autenticada
                 .anyRequest().authenticated()
